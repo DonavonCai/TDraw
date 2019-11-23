b62b36e0bcf7fc2ab64f6ce48459255465e60131
Donavon Cai <donavoncai@yahoo.com>
2019-11-22 20:35:20 -0800

    adding Javafx to project

---
+++ src/timingdiagram/TDraw.java
@@ -0,0 +1,13 @@
+package timingdiagram;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TDraw extends Application {
+    public static void main(String args[]) {
+
+    }
+    public void start(Stage primaryStage) {
+
+    }
+}
