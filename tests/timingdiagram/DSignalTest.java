33d355479a2ec36fe5f5cd53ddd86c3c02b745a9
Donavon Cai <donavoncai@yahoo.com>
2020-02-16 20:42:13 -0800

    Adding unit tests

---
+++ tests/timingdiagram/DSignalTest.java
@@ -0,0 +1,24 @@
+package timingdiagram;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DSignalTest {
+
+    @Test
+    void draw() {
+    }
+
+    @Test
+    void h_line_flip() {
+    }
+
+    @Test
+    void in_between_edges() {
+    }
+
+    @Test
+    void edges_balanced() {
+    }
+}
