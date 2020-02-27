01117e6ebec6aff8f1aadac06af3d21239c8a526
Donavon Cai <donavoncai@yahoo.com>
2020-02-27 15:53:23 -0800

    Set up proper environment for test driven dev after many long and painful hours of googling

---
+++ test/timingdiagram/MousePressHandlerTest.java
@@ -0,0 +1,12 @@
+package timingdiagram;
+
+import org.junit.Test;
+import org.junit.BeforeClass;
+import org.junit.AfterClass;
+
+public class MousePressHandlerTest {
+    @Test
+    public void t() {
+        System.out.println("mpress handler");
+    }
+}
