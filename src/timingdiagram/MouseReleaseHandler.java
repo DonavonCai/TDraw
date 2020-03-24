d65fa6aad857bbd03fe87070afec7c4e6906ebdb
Donavon Cai <donavoncai@yahoo.com>
2020-03-24 14:58:06 -0700

    Edges dragged over are now deleted

--- src/timingdiagram/MouseReleaseHandler.java
+++ src/timingdiagram/MouseReleaseHandler.java
@@ -6,7 +6,8 @@ import javafx.scene.input.MouseButton;
 import java.util.Collections;
 import java.util.ArrayList;
 
-public class MouseReleaseHandler extends Handler {
+public class  MouseReleaseHandler extends Handler {
+
     MouseReleaseHandler(DSignal d) {
         super(d);
     }
@@ -21,21 +22,24 @@ public class MouseReleaseHandler extends Handler {
         super.d_sig.initial_direction = DSignal.Direction.NULL;
         super.d_sig.prev_mouse_coord = -1;
 
-//        int press = super.d_sig.initial_edge;
         int release = (int)event.getX();
 
-        if (event.getButton() == MouseButton.PRIMARY) {
-            super.d_sig.neg_edges.add(release);
-            Collections.sort(super.d_sig.neg_edges);
-            System.out.println("release: adding neg edge at: " + event.getX() + ", " + event.getY());
-        }
-        else if (event.getButton() == MouseButton.SECONDARY) {
-            super.d_sig.pos_edges.add(release);
-            Collections.sort(super.d_sig.pos_edges);
-            System.out.println("release: adding pos edge at: " + event.getX() + ", " + event.getY());
+        if (event.getButton() == MouseButton.PRIMARY || event.getButton() == MouseButton.SECONDARY) {
+            if (super.d_sig.initial_edge_is_neg) { // if initial edge is neg, closing edge is pos
+                super.d_sig.pos_edges.add(release);
+                Collections.sort(super.d_sig.pos_edges);
+                System.out.println("release: adding pos edge at: " + event.getX() + ", " + event.getY());
+            }
+            else {
+                super.d_sig.neg_edges.add(release);
+                Collections.sort(super.d_sig.neg_edges);
+                System.out.println("release: adding neg edge at: " + event.getX() + ", " + event.getY());
+            }
         }
 
         System.out.println(super.d_sig.pos_edges.size() + " pos edges");
         System.out.println(super.d_sig.neg_edges.size() + " neg edges");
+        System.out.println("first pos: " + super.d_sig.pos_edges.get(0));
+        System.out.println("first neg: " + super.d_sig.neg_edges.get(0));
     }
 }
