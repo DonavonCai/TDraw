b5a5404193eb85f604bda02fcc4c100c82642a32
Donavon Cai <donavoncai@yahoo.com>
2020-04-04 21:44:45 -0700

    Can now save diagram as png file

--- src/timingdiagram/TDraw.java
+++ src/timingdiagram/TDraw.java
@@ -4,13 +4,20 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 // padding, spacing, etc
 import javafx.geometry.Insets;
 // event handling
 import javafx.event.ActionEvent;
+// pdf
+import javafx.scene.image.WritableImage;
+import javafx.scene.SnapshotParameters;
+import javafx.embed.swing.SwingFXUtils;
+import javax.imageio.ImageIO;
+import java.io.File;
+import javafx.stage.FileChooser;
+import java.io.IOException;
 
 public class TDraw extends Application {
     // constants for styling
@@ -37,24 +44,48 @@ public class TDraw extends Application {
         VBox signal_field = new VBox();
         signal_handler.add_signal(signal_field);
 
-        // initialize add signal button
+        // initialize buttons
+        Button export_button = new Button("Export as PDF");
+        initialize_style(export_button);
+
+        export_button.setOnAction((ActionEvent event) -> {
+            System.out.println("saving!");
+            WritableImage image = signal_field.snapshot(new SnapshotParameters(), null);
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Save diagram to...");
+            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("PNG", "*.png"));
+
+            File file = fileChooser.showSaveDialog(primaryStage);
+
+            try {
+                ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", file);
+            } catch (IOException e) {
+                System.out.println("Failed to save.");
+                return;
+            }
+        });
+
         Button add_signal = new Button("Add Signal");
-        add_signal.setStyle(IDLE_BUTTON_STYLE);
+        initialize_style(add_signal);
 
-        add_signal.setOnMousePressed(e -> add_signal.setStyle(PRESSED_BUTTON_STYLE));
-        add_signal.setOnMouseReleased(e -> add_signal.setStyle(IDLE_BUTTON_STYLE));
-
-        add_signal.setOnAction((ActionEvent event) -> { // functionality for adding new signals
+        add_signal.setOnAction((ActionEvent event) -> {
             signal_handler.add_signal(signal_field);
         });
 
-        HBox buttons = new HBox(add_signal);
+        VBox buttons = new VBox(export_button, add_signal);
         buttons.setPadding(new Insets(10));
+        buttons.setSpacing(10.0);
 
         VBox page = new VBox(buttons, signal_field);
         root.getChildren().add(page);
 
         Scene diagram = new Scene(root, 900, 500);
         primaryStage.setScene(diagram);
+    }
+
+    void initialize_style(Button b) {
+        b.setStyle(IDLE_BUTTON_STYLE);
+        b.setOnMousePressed(e -> b.setStyle(PRESSED_BUTTON_STYLE));
+        b.setOnMouseReleased(e -> b.setStyle(IDLE_BUTTON_STYLE));
     }
 }
