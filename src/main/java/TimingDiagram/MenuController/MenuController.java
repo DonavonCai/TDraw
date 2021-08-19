9278c5017912aa0f92ebd8d276d8b6b6c49467ad
Donavon Cai <donavoncai@yahoo.com>
2021-08-19 15:39:30 -0700

    deleted TimingDiagram folder

--- src/main/java/TimingDiagram/MenuController/MenuController.java
+++
@@ -1,9 +0,0 @@
-package TimingDiagram.MenuController;
-
-import TimingDiagram.FXMLController;
-
-abstract class MenuController {
-    protected FXMLController parent;
-
-    public void setParent(FXMLController p) {parent = p;}
-}
